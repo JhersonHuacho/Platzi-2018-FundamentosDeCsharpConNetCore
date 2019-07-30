a637b412f8f191a00dc9dc1e5d581ce7485e6e47
Jherson <jhersonhuacho@gmail.com>
2019-07-30 11:59:10 -0500

    Introducción a la Programación Orientado a Objetos

---
+++ Etapa00/CoreEscuela/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CoreEscuela
+{
+    class Escuela
+    {
+        public string nombre;
+        public string direccion;
+        public int anioFundacion;
+        public string ceo;
+        public void Timbrar()
+        {
+            //Todo
+            Console.Beep(10000, 3000);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var miEscuela = new Escuela();
+            miEscuela.nombre = "Huacho Academy";
+            miEscuela.direccion = "Calle 890";
+            miEscuela.anioFundacion = 2012;
+            Console.WriteLine("TIMBRE");
+            miEscuela.Timbrar();
+            // Console.WriteLine("Hello World!");
+        }
+    }
+}
