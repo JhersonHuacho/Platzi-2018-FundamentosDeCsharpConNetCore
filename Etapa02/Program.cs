4c0e209c372ce5d324744308e96371c720bfaa01
Jherson <jhersonhuacho@gmail.com>
2019-07-30 19:15:05 -0500

    Etapa 02 Creando cursos para la escuela: 01. Creando otros objetos para nuestra escuela

---
+++ Etapa02/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using CoreEscuela.Entidades;
+
+namespace Etapa01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var escuela = new Escuela("Huacho Acadamy", 2012, TiposEscuela.Primaria,
+                ciudad: "Loreto", pais: "Perú");
+            escuela.Pais = "Perú";
+            escuela.Ciudad = "Iquitos";
+
+            var curso1 = new Curso()
+            {
+                Nombre = "101"
+            };
+            var curso2 = new Curso()
+            {
+                Nombre = "201"
+            };
+            var curso3 = new Curso()
+            {
+                Nombre = "301"
+            };
+
+            Console.WriteLine(escuela);
+            System.Console.WriteLine("=====================");
+            Console.WriteLine(curso1.Nombre + " , " + curso1.UniqueId);
+            Console.WriteLine($"{curso2.Nombre} , {curso2.UniqueId}");
+            Console.WriteLine(curso3);
+        }
+    }
+}
